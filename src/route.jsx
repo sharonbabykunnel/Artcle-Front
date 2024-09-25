3d156498e14d5bc0227c3f052815c50aaef4f0b7
Sharon <sharonkunnel2003@gmail.com>
2024-09-25 17:00:19 +0530

    25.09.3

--- src/route.jsx
+++ src/route.jsx
@@ -1,11 +1,11 @@
 import {createBrowserRouter, createRoutesFromElements, Route} from 'react-router-dom'
 import App from './App'
-import SignUpPage from './pages/SignupPage';
-import HomePage from './pages/HomePage';
-import SignInPage from './pages/SignInPage';
-import PrivatePages from './middlewares/PrivatePages';
-import ProfilePage from './pages/ProfilePage';
-import MyArticlePage from './pages/MyArticlePage';
+import SignUpPage from './pages/SignupPage.jsx';
+import HomePage from './pages/HomePage.jsx';
+import SignInPage from './pages/SignInPage.jsx';
+import PrivatePages from './middlewares/PrivatePagesjsx';
+import ProfilePage from './pages/ProfilePage.jsx';
+import MyArticlePage from './pages/MyArticlePage,jsx';
 
 const routes = createBrowserRouter(
     createRoutesFromElements(
